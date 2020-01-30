2e115c5f078d4b0dc08da78b2a8e385d076bad31
Viniciusalves17 <60370253+Viniciusalves17@users.noreply.github.com>
2020-01-30 19:37:16 -0300

    Add files via upload

---
+++ Telas/Telas/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telas
+{
+    class Usuario
+    {
+        public int id { get; set;}
+        public String Nome { get; set;}
+        public String email { get; set;}
+        public String senha { get; set;}
+        public String usuario { get; set;}
+        public Boolean status { get; set;}
+        public int id_perfil { get; set;}
+
+    }
+}
---
+++ Telas/Telas/View/FrmLogin.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Telas
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.Usuario = new System.Windows.Forms.Label();
+            this.Txt_Usuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txt_senha = new System.Windows.Forms.TextBox();
+            this.Btn_Salvar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Location = new System.Drawing.Point(127, 40);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(43, 13);
+            this.Usuario.TabIndex = 0;
+            this.Usuario.Text = "Usuario";
+            // 
+            // Txt_Usuario
+            // 
+            this.Txt_Usuario.Location = new System.Drawing.Point(98, 56);
+            this.Txt_Usuario.Name = "Txt_Usuario";
+            this.Txt_Usuario.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Usuario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Senha";
+            // 
+            // Txt_senha
+            // 
+            this.Txt_senha.Location = new System.Drawing.Point(98, 109);
+            this.Txt_senha.Name = "Txt_senha";
+            this.Txt_senha.Size = new System.Drawing.Size(100, 20);
+            this.Txt_senha.TabIndex = 3;
+            // 
+            // Btn_Salvar
+            // 
+            this.Btn_Salvar.Location = new System.Drawing.Point(57, 150);
+            this.Btn_Salvar.Name = "Btn_Salvar";
+            this.Btn_Salvar.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Salvar.TabIndex = 4;
+            this.Btn_Salvar.Text = "Salvar";
+            this.Btn_Salvar.UseVisualStyleBackColor = true;
+            this.Btn_Salvar.Click += new System.EventHandler(this.Btn_Salvar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(156, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(293, 233);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Btn_Salvar);
+            this.Controls.Add(this.Txt_senha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Txt_Usuario);
+            this.Controls.Add(this.Usuario);
+            this.Name = "FrmLogin";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.TextBox Txt_Usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txt_senha;
+        private System.Windows.Forms.Button Btn_Salvar;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
